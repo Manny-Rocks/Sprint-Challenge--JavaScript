72e5681fb90b7d8029d259cb1313bc4aa26985c8
Emmanuel Tanifum <etantanifum@gmail.com>
2019-05-10 11:37:12 -0400

    Created cuboid object tat used the cuboidMaker constructor

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -54,7 +54,11 @@ surfaceArea () {
 */
 
 
-
+const cuboid = new CuboidMaker({
+  length: 4,
+  width: 5,
+  height: 5
+})
 
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
